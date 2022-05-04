3582238458ddfda3f874f00f40c1cf5d47031bff
Arthur Chiu <arthurchiu@hkrita.com>
2022-05-04 17:50:29 +0800

    ReadAttribute Class update

--- hkrita_robot/UR/accessor/ReadAttribute.cs
+++ hkrita_robot/UR/accessor/ReadAttribute.cs
@@ -1,13 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace hkrita_robot.UR.accessor
 {
-    public class ReadAttribute<T> 
+    public class ReadAttribute<T>
     {
+        private class AutoChangeObservable : IObservable<T>
+        {
+            public IDisposable Subscribe(IObserver<T> observer)
+            {
+                throw new NotImplementedException();
+            }
+        }
 
+        IObservable<T> mObservable = new AutoChangeObservable();
+        private ReaderWriterLock mLock = new ReaderWriterLock();
+        private T mObject;
+        private TypeInfo mType;
+
+        public T get()
+        {
+            lock (mLock)
+            {
+                try
+                {
+                    if (mObject == null) return default;
+
+                }
+                catch (Exception ex) { }
+            }
+            return mObject;
+                    
+        }
     }
 }
