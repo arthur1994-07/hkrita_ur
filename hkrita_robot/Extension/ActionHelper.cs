9fe19bbd7dfe7e9837a68d5cecdd02a8775bd543
Arthur Chiu <arthurchiu@hkrita.com>
2022-05-12 16:05:26 +0800

    add ActionHelper class

---
+++ hkrita_robot/Extension/ActionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hkrita_robot.Extension
+{
+    public class ActionHelper
+    {
+        private Action<object> mAction;
+        private object mTarget;
+
+        public static void SetAction(Action<object> action, object target)
+        {
+            action.Invoke(target);
+        }
+
+
+    }
+}
